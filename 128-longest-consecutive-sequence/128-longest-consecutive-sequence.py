6f4726f1eaffd18608bd87e639282bae87d12323
Ritesh Saha <riteshsaha214@gmail.com>
2022-05-19 14:24:06 +0530

    Time: 448 ms (46.41%), Space: 33.4 MB (7.99%) - LeetHub

--- 128-longest-consecutive-sequence/128-longest-consecutive-sequence.py
+++ 128-longest-consecutive-sequence/128-longest-consecutive-sequence.py
@@ -5,16 +5,24 @@ class Solution:
         visited = set()
         nums = set(nums)
         
-        def dfs(u, ln = 1):
-            visited.add(u)
-            adj_verts = [u - 1, u + 1]
-            for v in adj_verts:
-                if v in nums and v not in visited:
-                    ln = dfs(v, ln + 1)
+        def bfs(node):
+            queue = collections.deque()
+            queue.append(node)
+            visited.add(node)
+            ln = 0
+            
+            while queue:
+                u = queue.popleft()
+                ln += 1
+                adj_vrts = [u - 1, u + 1]
+                for v in adj_vrts:
+                    if v in nums and v not in visited:
+                        queue.append(v)
+                        visited.add(v)
             return ln
         
         mx_ln = 0
         for num in nums:
             if num not in visited:
-                mx_ln = max(mx_ln, dfs(num))
+                mx_ln = max(mx_ln, bfs(num))
         return mx_ln
