11fe4eb2bb9c57729022bb29ddeec83ec7bdc12f
Ritesh Saha <riteshsaha214@gmail.com>
2022-05-19 13:30:12 +0530

    Time: 83 ms (28.46%), Space: 14.4 MB (97.37%) - LeetHub

--- 242-valid-anagram/242-valid-anagram.py
+++ 242-valid-anagram/242-valid-anagram.py
@@ -1,14 +1,13 @@
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-        # If two string have exactly the same number of characters of each type, than they are anagrams.
+        # If two strings are permutations of each other, than they are anagrams.
         if len(s) != len(t): return False
         mp = collections.defaultdict(int)
         
         for i in range(len(s)):
             mp[s[i]] += 1
             mp[t[i]] -= 1
-        
-        for ch, cnt in mp.items():
-            if cnt != 0: return False
-        
+            
+        for key, val in mp.items():
+            if val != 0: return False
         return True
