b2f84047665a0b7ba9f24ff9832ff5c26e8530e6
Ritesh Saha <riteshsaha214@gmail.com>
2022-05-19 13:34:13 +0530

    Time: 67 ms (83.18%), Space: 15.2 MB (49.42%) - LeetHub

--- 1-two-sum/1-two-sum.py
+++ 1-two-sum/1-two-sum.py
@@ -1,14 +1,11 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        # Initialize a map
         mp = {}
         
+        # If (target - current_num) exists in the map, than return (target-current_num, current_num).
         for i, num in enumerate(nums):
-            # Check if the required number exists in the map.
             required = target - num
-            # If it exists in the map, than we have found the unique solution.
             if required in mp: return [i, mp[required]]
-            # Otherwise map the num to its index.
             mp[num] = i
-            
-        return None # This line will probably never get executed because solution is guranteed to exist. 
+        
+        return None
