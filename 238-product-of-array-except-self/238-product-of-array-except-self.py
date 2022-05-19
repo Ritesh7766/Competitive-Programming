71c8c69d237521eebb2fc427c362ff94b1d8318c
Ritesh Saha <riteshsaha214@gmail.com>
2022-05-19 13:50:36 +0530

    Time: 274 ms (61.64%), Space: 22.3 MB (23.89%) - LeetHub

--- 238-product-of-array-except-self/238-product-of-array-except-self.py
+++ 238-product-of-array-except-self/238-product-of-array-except-self.py
@@ -1,15 +1,16 @@
 class Solution:
     def productExceptSelf(self, nums: List[int]) -> List[int]:
-        n = len(nums)
-        prefix, postfix = [0] * n, [0] * n
+        # Precompute the cumulative product excluding the current element [Lets call this prefix.
+        # Do the same with nums.reverse [Lets call this postfix].
+        # For each element return prefix * postfix for that element.
         
-        prefix[0] = 1
+        n = len(nums)
+        prefix, postfix = [1] * n, [1] * n
+        
         for i in range(1, n):
             prefix[i] = prefix[i - 1] * nums[i - 1]
-        
-        postfix[-1] = 1
+            
         for i in range(n - 2, -1, -1):
             postfix[i] = postfix[i + 1] * nums[i + 1]
         
         return [prefix[i] * postfix[i] for i in range(n)]
-        
