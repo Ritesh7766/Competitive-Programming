bc88b49c640a253f608be1fa3dc87588c0c427da
Ritesh Saha <riteshsaha214@gmail.com>
2022-05-19 14:17:06 +0530

    Time: 454 ms (46.04%), Space: 33.6 MB (7.50%) - LeetHub

--- 128-longest-consecutive-sequence/128-longest-consecutive-sequence.py
+++ 128-longest-consecutive-sequence/128-longest-consecutive-sequence.py
@@ -1,11 +1,27 @@
 class Solution:
     def longestConsecutive(self, nums: List[int]) -> int:
+        # Let each element be a vertex of a graph.
+        # Each vertex v will be adjacent to at most two vertices, (v - 1) and (v + 1).
+        visited = set()
         nums = set(nums)
+        
+        def dfs(node):
+            stack = collections.deque()
+            stack.appendleft(node)
+            ln = 0
+            
+            while stack:
+                u = stack.popleft()
+                visited.add(u)
+                ln += 1
+                adj_vrts = [u - 1, u + 1]
+                for v in adj_vrts:
+                    if v in nums and v not in visited:
+                        stack.appendleft(v)
+            return ln
         
         mx_ln = 0
         for num in nums:
-            if (num + 1) in nums: continue
-            ln = 0
-            while (num - ln) in nums: ln += 1
-            mx_ln = max(mx_ln, ln)
+            if num not in visited:
+                mx_ln = max(mx_ln, dfs(num))
         return mx_ln
