8cd00e980b3fd6aa2d3b654e71d3e225dd10ea4d
Ritesh Saha <riteshsaha214@gmail.com>
2022-05-21 00:03:44 +0530

    Time: 65 ms (94.11%), Space: 13.9 MB (68.27%) - LeetHub

--- 567-permutation-in-string/567-permutation-in-string.py
+++ 567-permutation-in-string/567-permutation-in-string.py
@@ -1,11 +1,29 @@
 class Solution:
     def checkInclusion(self, s1: str, s2: str) -> bool:
-        mp1 = [0] * 26
+        if len(s1) > len(s2): return False
         
-        for ch in s1: mp1[ord(ch) - ord('a')] += 1
+        mp1, mp2 = [0] * 26, [0] * 26
+        for i in range(len(s1)):
+            mp1[ord(s1[i]) - ord('a')] += 1
+            mp2[ord(s2[i]) - ord('a')] += 1
         
-        for i in range(len(s2) - len(s1) + 1):
-            mp2 = [0] * 26
-            for ch in s2[i : i + len(s1)]: mp2[ord(ch) - ord('a')] += 1
-            if tuple(mp1) == tuple(mp2): return True
-        return False
+        matches = 0
+        for i in range(26):
+            matches += 1 if mp1[i] == mp2[i] else 0
+        
+        l = 0
+        for r in range(len(s1), len(s2)):
+            if matches == 26: return True
+            
+            index = ord(s2[r]) - ord('a')
+            mp2[index] += 1
+            if mp1[index] == mp2[index]: matches += 1
+            elif mp1[index] + 1 == mp2[index]: matches -= 1
+            
+            index = ord(s2[l]) - ord('a')
+            mp2[index] -= 1
+            if mp1[index] == mp2[index]: matches += 1
+            elif mp1[index] - 1 == mp2[index]: matches -= 1
+            l += 1
+        return matches == 26
+            
