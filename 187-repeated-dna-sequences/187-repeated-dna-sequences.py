97a39b0137fdceb4e59af910013b6af59a14df39
Ritesh Saha <riteshsaha214@gmail.com>
2022-05-22 22:06:37 +0530

    Time: 88 ms (61.01%), Space: 27.6 MB (35.87%) - LeetHub

--- 187-repeated-dna-sequences/187-repeated-dna-sequences.py
+++ 187-repeated-dna-sequences/187-repeated-dna-sequences.py
@@ -1,6 +1,6 @@
 class Solution:
     def findRepeatedDnaSequences(self, s: str) -> List[str]:
         mp = collections.defaultdict(int)
-        for i in range(len(s) - 10 + 1):
+        for i in range(len(s) - 9):
             mp[s[i : i + 10]] += 1
         return [key for key, value in mp.items() if value > 1]
