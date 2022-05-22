8b0db3edea8d25ad8286e6f96c57a3c19f75b6bf
Ritesh Saha <riteshsaha214@gmail.com>
2022-05-22 19:23:13 +0530

    Time: 133 ms (46.91%), Space: 14.1 MB (31.58%) - LeetHub

--- 567-permutation-in-string/567-permutation-in-string.py
+++ 567-permutation-in-string/567-permutation-in-string.py
@@ -2,7 +2,8 @@ class Solution:
     def checkInclusion(self, s1: str, s2: str) -> bool:
         if len(s1) > len(s2): return False
         key, mp = [0] * 26, [0] * 26
-        for c in s1: key[ord(c) - ord('a')] += 1
+        for c in s1: 
+            key[ord(c) - ord('a')] += 1
         for i in range(len(s1)): 
             mp[ord(s2[i]) - ord('a')] += 1
         
