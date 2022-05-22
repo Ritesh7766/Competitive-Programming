843c6d3ac7448b3d30c7de7e297449fdebe13807
Ritesh Saha <riteshsaha214@gmail.com>
2022-05-22 18:50:01 +0530

    Time: 218 ms (36.16%), Space: 14.1 MB (18.95%) - LeetHub

--- 424-longest-repeating-character-replacement/424-longest-repeating-character-replacement.py
+++ 424-longest-repeating-character-replacement/424-longest-repeating-character-replacement.py
@@ -1,12 +1,12 @@
 class Solution:
     def characterReplacement(self, s: str, k: int) -> int:
-        mp = collections.defaultdict(int)
-        left, res = 0, 0
+        mp, l = [0] * 26, 0
+        mx_ln = 0
+        for r in range(len(s)):
+            mp[ord(s[r]) - ord('A')] += 1
+            while ((r - l + 1) - max(mp)) > k:
+                mp[ord(s[l]) - ord('A')] -= 1
+                l += 1
+            mx_ln = max(mx_ln, r - l + 1)
+        return mx_ln
         
-        for right in range(len(s)):
-            mp[s[right]] += 1
-            while ((right - left + 1) - max(mp.values()) ) > k:
-                mp[s[left]] -= 1
-                left += 1
-            res = max(res, right - left + 1)
-        return res
