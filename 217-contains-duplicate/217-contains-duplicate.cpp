e018277c043c0adc8018f42c234d03ef4d8c8bdb
Ritesh Saha <riteshsaha214@gmail.com>
2022-05-23 22:43:38 +0530

    Time: 145 ms (30.88%), Space: 51.4 MB (51.60%) - LeetHub

---
+++ 217-contains-duplicate/217-contains-duplicate.cpp
@@ -0,0 +1,16 @@
+#include <unordered_set>
+
+using namespace std;
+
+class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums) {
+        unordered_set<int> set;
+        for (int i = 0; i < nums.size(); i++) {
+            if (set.count(nums[i]) == 1) 
+                return true;
+            set.insert(nums[i]);
+        }
+        return false;
+    }
+};
