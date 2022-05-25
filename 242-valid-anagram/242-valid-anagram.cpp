ed6d35ca37c2744031d23c9cd58ef0991074c7b1
Ritesh Saha <riteshsaha214@gmail.com>
2022-05-25 17:30:01 +0530

    Time: 12 ms (59.55%), Space: 7.3 MB (46.80%) - LeetHub

--- 242-valid-anagram/242-valid-anagram.cpp
+++ 242-valid-anagram/242-valid-anagram.cpp
@@ -1,4 +1,4 @@
-#include <unordered_map>
+#include <vector>
 
 using namespace std;
 
@@ -7,19 +7,15 @@ public:
     bool isAnagram(string s, string t) {
         if (s.length() != t.length())
             return false;
-        unordered_map<char, int> map;
+        vector<int> map(26, 0);
         
         for (int i = 0; i < s.length(); i++) {
-            if (!map.count(s[i]))
-                map[s[i]] = 0;
-            if (!map.count(s[i]))
-                map[t[i]] = 0;
-            map[s[i]] += 1;
-            map[t[i]] -= 1;
+            map[s[i] - 'a'] += 1;
+            map[t[i] - 'a'] -= 1;
         }
         
-        for (auto item : map)
-            if (item.second != 0)
+        for (auto count : map)
+            if (count != 0) 
                 return false;
         
         return true;
