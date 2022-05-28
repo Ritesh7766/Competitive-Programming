4ca0ac8a12fa53b1f43a30a297f89549da3c751b
Ritesh Saha <riteshsaha214@gmail.com>
2022-05-28 21:12:23 +0530

    Time: 23 ms (9.63%), Space: 14.1 MB (37.72%) - LeetHub

---
+++ 733-flood-fill/733-flood-fill.cpp
@@ -0,0 +1,36 @@
+#include <vector>
+#include <iostream>
+
+using namespace std;
+
+class Solution {
+public:
+    vector<pair<int, int> > get_adjacent_vertices(const int rows, const int cols, pair<int, int> u) {
+        vector<pair<int, int> > dirs{make_pair(1, 0), make_pair(0, 1), make_pair(-1, 0), make_pair(0, -1)};
+        vector<pair<int, int> > adjacent_vertices;
+        for (pair<int, int> dir : dirs) {
+            int x = dir.first + u.first;
+            int y = dir.second + u.second;
+            if (0 <= x && x < rows && 0 <= y && y < cols) 
+                adjacent_vertices.push_back(make_pair(x, y) ); 
+        }
+        return adjacent_vertices;
+    }
+    
+    void dfs(vector<vector<int>>& image, pair<int, int> u, int newColor, int initial_color) {
+        image[u.first][u.second] = newColor;
+        vector<pair<int, int> > adjacent_vertices = get_adjacent_vertices(image.size(), image[0].size(), u);
+        for (pair<int, int> v : adjacent_vertices) 
+            if (image[v.first][v.second] == initial_color)
+                dfs(image, v, newColor, initial_color);
+        return;
+    }
+    
+    vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int newColor) {
+        if (image[sr][sc] == newColor)
+            return image;
+        int initial_color = image[sr][sc];
+        dfs(image, make_pair(sr, sc), newColor, initial_color);
+        return image;
+    }
+};
