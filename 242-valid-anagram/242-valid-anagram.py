8d9c33da76547edcffa2b0adc05bd490be6eb31a
Ritesh Saha <riteshsaha214@gmail.com>
2022-06-04 20:53:25 +0530

    Time: 62 ms (62.86%), Space: 14.5 MB (34.53%) - LeetHub

--- 242-valid-anagram/242-valid-anagram.py
+++ 242-valid-anagram/242-valid-anagram.py
@@ -1,13 +1,13 @@
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-        # If two strings are permutations of each other, than they are anagrams.
         if len(s) != len(t): return False
+        
         mp = collections.defaultdict(int)
         
         for i in range(len(s)):
-            mp[s[i]] += 1
-            mp[t[i]] -= 1
+            mp[ord(s[i]) - ord('a')] += 1
+            mp[ord(t[i]) - ord('a')] -= 1
             
-        for key, val in mp.items():
-            if val != 0: return False
+        for _, count in mp.items():
+            if count != 0: return False
         return True
