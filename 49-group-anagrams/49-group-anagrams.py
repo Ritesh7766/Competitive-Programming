3d08a18a9e0824c610f33f5bdfca188933592f0d
Ritesh Saha <riteshsaha214@gmail.com>
2022-06-04 20:58:14 +0530

    Time: 160 ms (37.61%), Space: 19 MB (31.51%) - LeetHub

--- 49-group-anagrams/49-group-anagrams.py
+++ 49-group-anagrams/49-group-anagrams.py
@@ -1,12 +1,11 @@
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
-        # The number of characters grouped by alphabets will serve as a key to
-        # identify a set of anagrams.
-        
         mp = collections.defaultdict(list)
+        
         for str in strs:
             key = [0] * 26
-            for ch in str: key[ord(ch) - ord('a')] += 1
+            for ch in str:
+                key[ord(ch) - ord('a')] += 1
             mp[tuple(key)].append(str)
-        
-        return [group_anagram for _, group_anagram in mp.items()]
+            
+        return [group for _, group in mp.items()]
