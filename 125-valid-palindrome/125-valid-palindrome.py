ee00419120a61f498e58637f5ce1af8a8075adf3
Ritesh Saha <riteshsaha214@gmail.com>
2022-06-04 21:52:47 +0530

    Time: 77 ms (36.20%), Space: 21.2 MB (5.24%) - LeetHub

--- 125-valid-palindrome/125-valid-palindrome.py
+++ 125-valid-palindrome/125-valid-palindrome.py
@@ -1,6 +1,4 @@
 class Solution:
     def isPalindrome(self, s: str) -> bool:
         chars = [ch.lower() for ch in s if ch.isalnum()]
-        wrd = ''
-        for ch in chars: wrd += ch
-        return wrd == wrd[::-1]
+        return tuple(chars) == tuple(chars[::-1])
