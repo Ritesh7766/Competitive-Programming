7ef9c0ea852089dd4884fb09f5b20e950f3ec88d
Ritesh Saha <riteshsaha214@gmail.com>
2022-06-04 21:07:25 +0530

    Time: 204 ms (12.79%), Space: 19.7 MB (5.25%) - LeetHub

--- 347-top-k-frequent-elements/347-top-k-frequent-elements.py
+++ 347-top-k-frequent-elements/347-top-k-frequent-elements.py
@@ -1,21 +1,16 @@
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
-        # Bucket sort but instead of using the elements of the list as key
-        # we use the count of each element as key.
-        # An element of going to exists at most array.length times.
-        n = len(nums)
-        buckets = [[] for _ in range(n + 1)]
+        bucket = [[] for _ in range(len(nums) + 1)]
         mp = collections.defaultdict(int)
         
-        for num in nums:
-            mp[num] += 1
-            
-        for num, cnt in mp.items():
-            buckets[cnt].append(num)
+        for num in nums: mp[num] += 1
+        
+        for num, count in mp.items():
+            bucket[count].append(num)
             
         res = []
-        for i in range(n, -1, -1):
+        for i in range(len(nums), -1, -1):
             if k == 0: break
-            k -= len(buckets[i])
-            for num in buckets[i]: res.append(num)
+            k -= len(bucket[i])
+            for num in bucket[i]: res.append(num)
         return res
