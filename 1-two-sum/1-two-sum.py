4babfba0054d2d18882439b4b7b50362cd7c0c94
Ritesh Saha <riteshsaha214@gmail.com>
2022-06-04 20:54:57 +0530

    Time: 100 ms (52.98%), Space: 15.1 MB (64.34%) - LeetHub

--- 1-two-sum/1-two-sum.py
+++ 1-two-sum/1-two-sum.py
@@ -2,10 +2,10 @@ class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
         mp = {}
         
-        # If (target - current_num) exists in the map, than return (target-current_num, current_num).
         for i, num in enumerate(nums):
             required = target - num
-            if required in mp: return [i, mp[required]]
+            if required in mp:
+                return [mp[required], i]
             mp[num] = i
-        
+            
         return None
