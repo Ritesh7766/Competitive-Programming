bf134b93b850598683136a53511e4ee332f9f5e7
Ritesh Saha <riteshsaha214@gmail.com>
2022-06-12 14:27:52 +0530

    Time: 1457 ms (24.44%), Space: 30.1 MB (97.53%) - LeetHub

--- 2260-minimum-consecutive-cards-to-pick-up/2260-minimum-consecutive-cards-to-pick-up.py
+++ 2260-minimum-consecutive-cards-to-pick-up/2260-minimum-consecutive-cards-to-pick-up.py
@@ -2,11 +2,13 @@ from math import inf
 
 class Solution:
     def minimumCardPickup(self, cards: List[int]) -> int:
-        mp, mn = {}, inf
-        for i, card in enumerate(cards):
-            if card in mp:
-                mn = min(mn, i - mp[card] + 1)
-                mp[card] = i
-            else: mp[card] = i
-                
-        return mn if mn != inf else -1
+        hash_set, l = set(), 0
+        
+        mn = inf
+        for r in range(len(cards) ):
+            while cards[r] in hash_set:
+                mn = min(r - l + 1, mn)
+                hash_set.remove(cards[l])
+                l += 1
+            hash_set.add(cards[r])
+        return -1 if mn is inf else mn
