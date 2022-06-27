02ae86eecdcac128c9a3105f0e4a60932bafc71e
Ritesh Saha <riteshsaha214@gmail.com>
2022-06-27 09:04:44 +0530

    Time: 526 ms (96.35%), Space: 49.5 MB (68.78%) - LeetHub

--- 111-minimum-depth-of-binary-tree/111-minimum-depth-of-binary-tree.py
+++ 111-minimum-depth-of-binary-tree/111-minimum-depth-of-binary-tree.py
@@ -4,15 +4,16 @@
 #         self.val = val
 #         self.left = left
 #         self.right = right
-from math import inf
-
 class Solution:
     def minDepth(self, root: Optional[TreeNode]) -> int:
-        def min_depth(node):
-            if not node:
-                return inf
-            if not node.left and not node.right:
-                return 1
-            return min(min_depth(node.left), min_depth(node.right) ) + 1
-        depth = min_depth(root)
-        return 0 if depth is inf else depth
+        if not root: return 0
+        queue = collections.deque()
+        queue.append((root, 1))
+        
+        while queue:
+            u, depth = queue.popleft()
+            if not u.left and not u.right: 
+                return depth
+            if u.left: queue.append((u.left, depth + 1))
+            if u.right: queue.append((u.right, depth + 1))
+        return 0
