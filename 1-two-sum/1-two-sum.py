e1cd00bfcd91633844da2094b5c447602e7d2349
Ritesh Saha <riteshsaha214@gmail.com>
2022-07-16 09:04:08 +0530

    Time: 131 ms (39.78%), Space: 15.3 MB (24.50%) - LeetHub

--- 1-two-sum/1-two-sum.py
+++ 1-two-sum/1-two-sum.py
@@ -1,11 +1,9 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
         mp = {}
-        
         for i, num in enumerate(nums):
             required = target - num
             if required in mp:
                 return [mp[required], i]
             mp[num] = i
-            
         return None
