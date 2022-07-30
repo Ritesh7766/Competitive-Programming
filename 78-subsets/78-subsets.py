999b711638d66b9394fed6c5f5c5a9970fcb3560
Ritesh Saha <riteshsaha214@gmail.com>
2022-07-30 09:04:14 +0530

    Time: 40 ms (84.21%), Space: 14.2 MB (34.98%) - LeetHub

--- 78-subsets/78-subsets.py
+++ 78-subsets/78-subsets.py
@@ -1,5 +1,7 @@
 class Solution:
     def subsets(self, nums: List[int]) -> List[List[int]]:
+        '''
+        Recursive solution
         def sub(elm = 0, sub_set = [], power_set = []):
             if elm == len(nums):
                 return power_set + [sub_set]
@@ -7,5 +9,13 @@ class Solution:
             power_set = sub(elm + 1, sub_set, power_set)
             power_set = sub(elm + 1, sub_set + [nums[elm]], power_set)
             return power_set
-        
         return sub()
+        '''
+        power_set, n = [], len(nums)
+        for i in range(2 ** n):
+            sub_set = []
+            for k in range(n):
+                if i & (1 << k):
+                    sub_set.append(nums[k])
+            power_set.append(sub_set)
+        return power_set
